b1dcc5e32298e4c83d0f6998a6fcb28d7c55afa3
Andres12345-debug <julian2000mk1@gmail.com>
2026-06-14 10:11:12 -0500

    Solcuion errores vercel

--- src/app/public/components/componentsPlane/PlanesPage.tsx
+++ src/app/public/components/componentsPlane/PlanesPage.tsx
@@ -1,13 +1,9 @@
 
 import OnboardingSteps from './OnboardingSteps';
-import { useThemeContext } from "../../../shared/theme/ThemeConext";
 import TechDifferentiator from './TechDifferentiator';
 import { Box } from '@mui/system';
 
 const PlanesPage = () => {
-    const { mode } = useThemeContext();
-    const isDark = mode === 'dark';
-
     return (
         <Box>
             <OnboardingSteps />
--- src/app/public/pages/Welcome.tsx
+++ src/app/public/pages/Welcome.tsx
@@ -1,4 +1,4 @@
-import { Box, useTheme } from "@mui/material";
+import { Box } from "@mui/material";
 import { useTranslation } from "react-i18next";
 import SEO from "../../shared/SEO";
 import SectionHeader from "../components/componentsWelcome/SectionHeader";
